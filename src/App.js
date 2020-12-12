b12ad6f1483e67983eafa1e760cb6bbef0e69f69
angiegonza125 <69263578+angiegonza125@users.noreply.github.com>
2020-12-11 20:35:37 -0600

    parcial 4

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
-export default function App(){
-    return(
-        <h1>desde la app</h1>
-    )
+import React, { Suspense, lazy } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./App.css";
+
+// import AppRouter from "./routes/app.router";
+// import AdminRouter from "./routes/admin.router";
+
+const NotFound = lazy(() => import("./components/NotFound"));
+const AppRouter = lazy(() => import("./components/app.router"));
+const AdminRouter = lazy(() => import("./components/admin.router"));
+
+function App() {
+  return (
+    <Router>
+      {/* <div>hello aman</div> */}
+      <ul className="nav-links">
+        <li>
+          <Link to="/admin">inicio</Link>
+        </li>
+        <li>
+          <Link to="/home">portafolio</Link>
+        </li>
+      </ul>
+      <Suspense fallback={<div>cargando...</div>}>
+        <Switch>
+          <AppRouter />
+        </Switch>
+        <Switch>
+          <AdminRouter />
+        </Switch>
+        {/* <Route component={NotFound} /> */}
+      </Suspense>
+    </Router>
+  );
 }
+
+export default App;
---
+++ src/components/app.router.js
@@ -0,0 +1,28 @@
+import React, { Suspense, lazy } from "react";
+import { Route } from "react-router-dom";
+import Loadable from "react-loadable";
+import Loading from "../components/Loading";
+
+
+const About = lazy(() => import("../components/About"));
+const ContactUs = lazy(() => import("../components/ContactUs"));
+const HomeModule = lazy(() => import("../modules/home.module"));
+
+export const Home = Loadable({
+  loader: () => import("../components/Home"),
+  loading: Loading
+});
+
+export default function AppRouter() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Route exact path="/home" component={HomeModule} />
+      <Route exact path="/home/page" component={Home} />
+      <Route exact path="/home/about" component={About} />
+      <Route exact path="/home/about/:id" component={About} />
+      <Route path="/home/contactus" component={ContactUs} />
+    </Suspense>
+  );
+}
+
+// export default AppRouter;
